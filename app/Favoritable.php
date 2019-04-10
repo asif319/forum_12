8849e741d0dd3ce72d0fdf59efb68d600817d254
Md Asifur Rahman <Md Asifur Rahman>
2019-04-10 22:55:44 +0600

    Few bug fixes

--- app/Favoritable.php
+++ app/Favoritable.php
@@ -7,6 +7,13 @@ namespace App;
 trait Favoritable
 {
 
+    protected static function bootFavoritable()
+    {
+        static::deleting(function ($model) {
+           $model->favorites->each->delete();
+        });
+    }
+
     public function isFavorited()
     {
         return !!$this->favorites->where('user_id', auth()->id())->count();
@@ -41,6 +48,7 @@ trait Favoritable
     {
         $attributes = ['user_id' => auth()->id()];
 
-        $this->favorites()->where($attributes)->delete();
+        $this->favorites()->where($attributes)->get()->each->delete();
+
     }
 }
